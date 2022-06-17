6d7f1638ccc23a8a84bc409ab26967f3069a6d52
smartzhao <smartzhao@tencent.com>
2022-06-17 12:12:28 +0800

    feat: web-04
    router group

--- gee/gee/gee.go
+++ gee/gee/gee.go
@@ -1,17 +1,61 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
 type HandlerFunc func(*Context)
 
-type Engine struct {
-	router *router
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // 支持中间件
+	parent      *RouterGroup  // 支持嵌套
+	engine      *Engine       // 共享 Engine 实例
 }
 
+type Engine struct {
+	*RouterGroup
+	router *router
+	groups []*RouterGroup // 存储所有的 group
+}
+
+// New is the constructor of gee.Engine
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	routerGroup := &RouterGroup{
+		engine: engine,
+	}
+	engine.RouterGroup = routerGroup
+	engine.groups = []*RouterGroup{routerGroup}
+	return engine
+}
+
+// Group is defined to create a new RouterGroup
+// remember all groups share the same Engine instance
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 func (e *Engine) Run(addr string) (err error) {
